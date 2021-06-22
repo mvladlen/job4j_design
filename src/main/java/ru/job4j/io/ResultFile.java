271d1f3f11dc7e4be26aea42dd18a1990ab778c3
mvladlen <vladlen@ufanet.ru>
2021-06-22 18:03:52 +0500

    0.1. FileOutputStream. [#252488 #169631] fix

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,7 +7,7 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i < 10; i++)
                 for (int j = 1; j < 10; j++) {
-                    out.write(("> " + i + "*" + j + " = " + i * j + "\n").getBytes());
+                    out.write(("> " + i + "*" + j + " = " + i * j + System.lineSeparator()).getBytes());
                 }
         } catch (Exception e) {
             e.printStackTrace();
