e325ec10397eee9c69660b288d069c54526fa0d9
mvladlen <vladlen@ufanet.ru>
2021-06-27 11:43:32 +0500

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static ru.job4j.io.Analizy.unavailable;
+
+public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void drop() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02\n"
+                    + "200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "500 10:59:01\n"
+                    + "400 11:01:02\n"
+                    + "200 11:02:02");
+        }
+
+        unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:0111:01:02;11:02:0210:57:01;11:02:02"));
+    }
+
+}
