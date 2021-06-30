82e067440dd7ed424c2669b6a748a06f77bef78a
mvladlen <vladlen@ufanet.ru>
2021-06-30 16:26:11 +0500

    6. Кодировка. [#862] на самом деле про чат2

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -9,18 +9,11 @@ public class ConsoleChat {
     private static final String CONTINUE = "продолжить";
     private final String path;
     private final String botAnswers;
-    private final List<String> dialog = new LinkedList<>();
-    private final List<String> answers = new ArrayList<>();
     Scanner console = new Scanner(System.in);
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
-        try (BufferedReader read = new BufferedReader(new FileReader(botAnswers))) {
-            read.lines().forEach(answers::add);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
@@ -28,34 +21,51 @@ public class ConsoleChat {
         cc.run();
     }
 
+    private void readAnswers(List<String> answers, String infile) {
+        try (BufferedReader read = new BufferedReader(new FileReader(infile))) {
+            read.lines().forEach(answers::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void run() {
         String userString;
         boolean answerPermission = true;
         Random random = new Random();
-//
+        List<String> dialog = new LinkedList<>();
+        List<String> answers = new ArrayList<>();
+
+        readAnswers(answers, this.botAnswers);
         do {
             userString = console.nextLine();
             dialog.add(userString);
-            if (userString.equals(STOP) || userString.equals(OUT)) answerPermission = false;
-            if (userString.equals(CONTINUE)) answerPermission = true;
+            if (userString.equals(STOP) || userString.equals(OUT)) {
+                answerPermission = false;
+            }
+            if (userString.equals(CONTINUE)) {
+                answerPermission = true;
+            }
 
             if (answerPermission) {
                 String answer = answers.get(random.nextInt(answers.size()));
                 dialog.add(answer);
                 System.out.println(answer);
             }
-
         } while (!userString.equals(OUT));
+        writeLog(dialog, this.path);
+    }
 
-// запись диалога по окончании чата
+    private void writeLog(List<String> log, String path) {
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
                         new FileOutputStream(path)
                 ))) {
-            dialog.forEach(out::println);
+            log.forEach(out::println);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
