e13b853bcf2b263f59858c832b65574a325c14bf
mvladlen <vladlen@ufanet.ru>
2021-07-05 08:22:30 +0500

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,10 +1,22 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class EchoServer {
+
+    private static String getCommand(String getString) {
+        var begin = getString.indexOf("/?msg=");
+        var end = getString.indexOf(' ', begin);
+        String ret = getString.substring(begin + 6, end);
+        return ret;
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -12,14 +24,19 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.equals("GET /?msg=Bye HTTP/1.1")) {
-                            return;
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String str = in.readLine();
+                    System.out.println(str);
+                    if (str.contains("?msg")) {
+                        if (getCommand(str).equals("Exit")) {
+                            server.close();
+                        }
+                        if (getCommand(str).equals("Hello")) {
+                            out.write("Hello".getBytes());
+                        } else {
+                            out.write(getCommand(str).getBytes());
                         }
                     }
-
                 }
             }
         }
